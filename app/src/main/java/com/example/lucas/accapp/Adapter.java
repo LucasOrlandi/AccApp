f4768356444a8f57943d82eb36ae67f6fabb94b2
Lucas Orlandi <lucas.orlandi.o@gmail.com>
2017-03-13 14:53:08 -0300

    MainActivity parcialmente pronta

---
+++ app/src/main/java/com/example/lucas/accapp/Adapter.java
@@ -0,0 +1,8 @@
+package com.example.lucas.accapp;
+
+/**
+ * Created by lucas on 3/10/17.
+ */
+
+public class Adapter {
+}
--- app/src/main/java/com/example/lucas/accapp/MainActivity.java
+++ app/src/main/java/com/example/lucas/accapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.lucas.accapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button_show_all = (Button) findViewById(R.id.button_show_all);
+        final Button button_show_random = (Button) findViewById(R.id.button_show_random);
+
+        button_show_all.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, ListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button_show_random.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, ListRandomActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
